1bd7672859a6aab79f220620030d965488345f25
Tuomas Mellin <tuomas@jmellin.net>
2023-11-02 18:57:40 +0200

    Added API Router to the project and also simplified the code.

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require('express');
 const app = express()
 const port = 3000;
 require('dotenv').config();
-// MySQL database stuff
+const apiRouter = require('./api/routes/routes')
+
+
+
+//////////////////////////
+//Database stuff is here//
+/////////////////////////
 const conn = require('./util/sql/connect');
 const connection = conn.connect;
 const db = require('./util/sql/storageController');
@@ -13,21 +19,18 @@ connection.connect(function(err) {
     console.log("Connected!");
 });
 
-app.post('/api/storevalues', (req, res) => {
-    const timestamp = req.body.timestamp;
-    const temperature = req.body.temperature;
-    const airPressure = req.body.airPressure;
-    const humidity = req.body.humidity;
-    db.storeValues(timestamp, temperature, airPressure, humidity);
-    return "Values stored!";
-});
 
-app.get('/api/getvalues', (req, res) => {
-    db.getValues();
-    return "Values retrieved!";
-});
 
-//app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+////////////////////
+//Routers are here//
+////////////////////
+app.use('/api', apiRouter);
+
+
+
+//////////////////////////
+//Required stuff is here//
+//////////////////////////
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
 // Open index.html if the user goes to an url that doesn't exist
