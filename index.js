bdab3014d113eb8cce70a6c66248ef8d02317c9a
Tuomas Mellin <tuomas@jmellin.net>
2023-11-02 21:28:22 +0200

    Added Sequelize as a way to access, manage and manipulate our MariaDB database.
    Refer to Sequelize documentation for help on how to use it.

--- index.js
+++ index.js
@@ -2,24 +2,10 @@ const express = require('express');
 const app = express()
 const port = 3000;
 require('dotenv').config();
+const { sequelize } = require('./models')
 const apiRouter = require('./api/routes/routes')
 
-
-
-//////////////////////////
-//Database stuff is here//
-/////////////////////////
-const conn = require('./util/sql/connect');
-const connection = conn.connect;
-const db = require('./util/sql/storageController');
-
-// Try to create connection to the MySQL database and catch any errors that occur
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-});
-
-
+start();
 
 ////////////////////
 //Routers are here//
@@ -31,7 +17,15 @@ app.use('/api', apiRouter);
 //////////////////////////
 //Required stuff is here//
 //////////////////////////
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+
+async function start() {
+    app.listen(port, () => console.log(`App listening on port ${port}!`))
+        sequelize.authenticate().then(() => {
+            console.log('Connection to database has been established successfully.');
+        }).catch((error) => {
+            console.error('Unable to connect to the database: ', error);
+        });
+    }
 
 // Open index.html if the user goes to an url that doesn't exist
 app.use(function(req, res, next) {
---
+++ migrations/20231102183919-create-measurements.js
@@ -0,0 +1,33 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Measurements', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      temperature: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deletedAt: {
+        allowNull: true,
+        type: Sequelize.DATE,
+        default: null
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Measurements');
+  }
+};
