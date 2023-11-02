3a0e4eefe3ec8a705cc855b65ce106b27a349966
Tuomas Mellin <tuomas@jmellin.net>
2023-11-02 22:48:18 +0200

    Added folder for tests and added a single test file.

---
+++ tests/PrintAllTables.js
@@ -0,0 +1,46 @@
+// RUN THE SCRIPT BY RUNNING: node PrintAllTables.js
+
+const mysql = require('mysql');
+
+// Replace these with your own database credentials
+const dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_SCHEMA
+};
+
+const connection = mysql.createConnection(dbConfig);
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+
+    console.log('Connected to the MariaDB database');
+
+    // Query to retrieve the list of tables in the database
+    const query = 'SHOW TABLES';
+
+    connection.query(query, (err, results) => {
+        if (err) {
+            console.error('Error retrieving tables:', err);
+        } else {
+            console.log('Tables in the database:');
+            results.forEach((row) => {
+                const tableName = row[Object.keys(row)[0]];
+                console.log(tableName);
+            });
+        }
+
+        // Close the database connection
+        connection.end((err) => {
+            if (err) {
+                console.error('Error closing the database connection:', err);
+            } else {
+                console.log('Database connection closed');
+            }
+        });
+    });
+});
