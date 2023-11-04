b37f694bd00b0e409c0701e07df4d6b2df28c27a
Tuomas Mellin <tuomas@jmellin.net>
2023-11-04 12:46:45 +0200

    Added API endpoints to support fetching of measurements based on days.
    Now hardcoded 30, 60, 120.

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -14,4 +14,9 @@ router.get('/values', apiController.getValues);
 router.post('/values', apiController.storeValues);
 router.delete('/values', apiController.deleteValues);
 
+router.get('/values/last30', apiController.getLast30DaysValues);
+router.get('/values/last60', apiController.getLast60DaysValues);
+router.get('/values/last120', apiController.getLast120DaysValues);
+
+
 module.exports = router;
