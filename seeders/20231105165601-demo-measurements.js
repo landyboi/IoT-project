c9b8ccc16bb1f9e3384a7de3a7147fd1fcbb69eb
Tuomas Mellin <tuomas@jmellin.net>
2023-11-08 21:28:58 +0200

    Update seeders.

--- seeders/20231105165601-demo-measurements.js
+++ seeders/20231105165601-demo-measurements.js
@@ -7,32 +7,32 @@ module.exports = {
          {
            temperature: 15,
            createdAt: '2023-11-05 12:00:00',
-           updatedAt: '2023-11-05 12:00:00'
+           measuredAt: '2023-11-05 12:00:00'
          },
        {
          temperature: 25,
          createdAt: '2023-08-13 13:52:00',
-         updatedAt: '2023-08-13 13:52:00'
+         measuredAt: '2023-08-13 13:52:00'
        },
        {
          temperature: 28,
          createdAt: '2023-07-01 06:25:00',
-         updatedAt: '2023-07-01 06:25:00'
+         measuredAt: '2023-07-01 06:25:00'
        },
        {
          temperature: 42,
          createdAt: '2022-06-05 03:14:00',
-         updatedAt: '2022-06-05 03:14:00'
+         measuredAt: '2022-06-05 03:14:00'
        },
        {
          temperature: 3,
          createdAt: '2023-10-08 15:00:00',
-         updatedAt: '2023-10-08 15:00:00'
+         measuredAt: '2023-10-08 15:00:00'
        },
        {
          temperature: 12,
          createdAt: '2023-10-10 15:32:00',
-         updatedAt: '2023-10-10 15:32:00',
+         measuredAt: '2023-10-10 15:32:00',
          deletedAt: '2023-10-15 12:00:00'
        }
       ], {});
