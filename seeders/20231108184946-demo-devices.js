c95c760223eeabab67b4d499c85cd8bbb540b0ee
Tuomas Mellin <tuomas@jmellin.net>
2023-11-08 20:52:50 +0200

    Update seeder.

--- seeders/20231108184946-demo-devices.js
+++ seeders/20231108184946-demo-devices.js
@@ -30,6 +30,6 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    return queryInterface.bulkDelete('Measurements', null, {});
+    return queryInterface.bulkDelete('Devices', null, {});
   }
 };
