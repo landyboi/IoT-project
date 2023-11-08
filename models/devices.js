28d7e8b122e81e1cbbac1c3c58e73615ad449ced
“Tuomaaaas” <tuomas.mellin@metropolia.fi>
2023-11-08 11:16:51 +0200

    Create "Devices" table to the database

---
+++ models/devices.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Devices extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Devices.init({
+    name: DataTypes.STRING,
+    country: DataTypes.VARCHAR(3),
+    uuid: DataTypes.UUID,
+    deletedAt: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'Devices',
+  });
+  return Devices;
+};
