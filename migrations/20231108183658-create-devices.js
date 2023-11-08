3ad3b4b4897f2057d192423591a98732d0e56b6a
Tuomas Mellin <tuomas@jmellin.net>
2023-11-08 20:47:49 +0200

    Add "Devices" table to the database.

---
+++ migrations/20231108183658-create-devices.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Devices', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.DataTypes.INTEGER
+      },
+      name: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false
+      },
+      country: {
+        type: Sequelize.DataTypes.CHAR(3),
+        allowNull: false,
+        default: 'FIN'
+      },
+      uuid: {
+        type: Sequelize.DataTypes.UUID,
+        allowNull: false,
+        unique: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deletedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Devices');
+  }
+};
