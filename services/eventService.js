c815b2c22948881a60404824e51b1172e1a29a2f
Tuomas Mellin <tuomas@jmellin.net>
2023-11-15 15:09:35 +0200

    updated emailService

--- services/eventService.js
+++ services/eventService.js
@@ -1,8 +1,7 @@
 const emailService = require("./emailService");
 
 const sendWeatherEmail = async (data) => {
-    const result = await emailService.sendTemplateEmail('tuomas.mellin@metropolia.fi', 'Weather', 'd-663d7909b55f4ee98368c236e79b2dbb', data);
-    console.log(result)
+    return await emailService.sendTemplateEmail('tuomas.mellin@metropolia.fi', 'Weather', 'd-663d7909b55f4ee98368c236e79b2dbb', data);
 }
 
 module.exports = {
