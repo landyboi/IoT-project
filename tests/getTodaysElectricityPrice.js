dafac6244bde68beaa1c96134f75d7e33a2e7bac
“Tuomaaaas” <tuomas.mellin@metropolia.fi>
2023-11-26 22:08:06 +0200

    Add functions to electricity price fetcher.

--- tests/getTodaysElectricityPrice.js
+++ tests/getTodaysElectricityPrice.js
@@ -3,7 +3,7 @@ const electricityService = require('../services/electricityPriceFetcher');
 
 async function main() {
     try {
-        const result = await electricityService.getSpecificPriceForDate('2021-03-01', '00');
+        const result = await electricityService.getSpecificPriceForDate('2022-01-01', 12);
         console.log('Result: ', result);
     } catch (error) {
         console.error('Error: ', error);
