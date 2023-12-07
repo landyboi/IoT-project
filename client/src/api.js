c28f207f0b9e612ca247569baad7d4fb74db191f
Tuomas Mellin <tuomas@jmellin.net>
2023-12-07 22:53:33 +0200

    update.

--- client/src/api.js
+++ client/src/api.js
@@ -1,36 +1,46 @@
 import axios from 'axios';
+import moment from "moment";
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'http://localhost:8000/api',
     headers: {
-        'Content-Type': 'application/json',
         'key': 'tuomasclient'
     }
 });
 
 
-
-export const getLatestMeasurement = async (device) => {
+export const getLatestMeasurementFromDevice = async (device) => {
     const result = await api.get('/values/latest', {
         params: {
             id: device
         }
     });
 
-    return result.data
+    return result.data.data;
 }
+
 
 export const getAllDevices = async () => {
     const result = await api.get('/devices/client');
 
-    return result.data;
+    return result.data.data;
 };
+
 
 export const getTodaysMeasurementsForDevice = async (device) => {
-    const today = new Date();
-    const datestamp = today.getFullYear() + "-" + today.getMonth() + "-" + today.getDate();
+    const today = moment().format('YYYY-MM-DD');
 
-    const result = await api.get(`/values/${device}/${datestamp}`);
+    const testDate = '2023-11-15'
+    const result = await api.get(`/values/${device}/${testDate}`);
 
-    return result.data;
+    return result.data.data;
 };
+
+
+export const getLast5DaysAverageMeasurementsForDevice = async (device) => {
+    const today = moment().format('YYYY-MM-DD');
+    const day2 = moment().subtract(1, 'days').format('YYYY-MM-DD');
+    const day3 = moment().subtract(2, 'days').format('YYYY-MM-DD');
+    const day4 = moment().subtract(3, 'days').format('YYYY-MM-DD');
+    const day5 = moment().subtract(4, 'days').format('YYYY-MM-DD');
+}
