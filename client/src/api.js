e5a2dd54089dd98fdbc7b1c6e533daaa739acd5e
Tuomas Mellin <tuomas@jmellin.net>
2023-12-10 20:40:35 +0200

    Final commit.

--- client/src/api.js
+++ client/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import moment from "moment";
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: 'https://team5.ddns.net/api',
     headers: {
         'Content-Type': 'application/json',
         'key': 'tuomasclient'
--- index.js
+++ index.js
@@ -1,38 +1,46 @@
 const express = require('express');
-const cors = require('cors');
 const app = express()
-const port = 8000;
+const path = require('path');
+const port = 3000;
 const { sequelize } = require('./models')
 const apiRouter = require('./api/routes/routes')
 const bodyParser = require('body-parser')
 
-app.use(cors());
+
 
 //Make it possible to read the body of the request.
 app.use(bodyParser.json());
 
-////////////////////
+
+
+///////////////////////////
+//Frontend stuff is here//
+//////////////////////////
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+});
+
+
+
+/////////////////////
 //Routers are here//
 ////////////////////
 app.use('/api', apiRouter);
 
 
 
-//////////////////////////
+///////////////////////////
 //Required stuff is here//
 //////////////////////////
 function start() {
     app.listen(port, () => console.log(`App listening on port ${port}!`))
-        sequelize.authenticate().then(() => {
-            console.log('Connection to database has been established successfully.');
-        }).catch((error) => {
-            console.error('Unable to connect to the database: ', error);
-        });
-    }
-
-// Open index.html if the user goes to an url that doesn't exist
-app.use(function(req, res) {
-    res.status(404).sendFile(__dirname + '/index.html');
-});
+    sequelize.authenticate().then(() => {
+        console.log('Connection to database has been established successfully.');
+    }).catch((error) => {
+        console.error('Unable to connect to the database: ', error);
+    });
+}
 
 start();
